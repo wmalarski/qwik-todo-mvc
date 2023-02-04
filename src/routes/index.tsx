8f5e23bd8c4e5b6f21ad484ffb840f128a0fed4e
Wojciech Malarski <w.malarski@gmail.com>
2023-02-04 13:44:32 +0100

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,27 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <main>
+        <section />
+      </main>
+      <footer>
+        <a href="https://www.builder.io/" target="_blank">
+          Made with ♡ by Builder.io
+        </a>
+      </footer>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
